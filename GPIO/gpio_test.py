1cde4c3bab33ded2c1fde6a869919b1cb68cc4a3
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-11 18:46:30 +0100

    added some prints for debugging

--- GPIO/gpio_test.py
+++ GPIO/gpio_test.py
@@ -21,19 +21,24 @@ if __name__ == '__main__':
     GPIO.setup(MOTOR_ENABLE_PIN, GPIO.OUT)
     GPIO.output(MOTOR_ENABLE_PIN, GPIO.HIGH)
 
+    print(f"Enable pin mode: {GPIO.gpio_function(MOTOR_ENABLE_PIN)}")
+
     time.sleep(10)
 
-    dc = 50
+    dc = 50.0
     freq = 2e3
     GPIO.setup(MOTOR_PWM_1_PIN, GPIO.OUT)
+    print(f"PWM 1 pin mode: {GPIO.gpio_function(MOTOR_PWM_1_PIN)}")
     pwm_1 = GPIO.PWM(MOTOR_PWM_1_PIN, freq)
 
     GPIO.setup(MOTOR_PWM_2_PIN, GPIO.OUT)
+    print(f"PWM 2 pin mode: {GPIO.gpio_function(MOTOR_PWM_2_PIN)}")
     pwm_2 = GPIO.PWM(MOTOR_PWM_2_PIN, freq)
-    # p.ChangeFrequency(freq)   # where freq is the new frequency in Hz
 
     pwm_1.start(dc)   # where dc is the duty cycle (0.0 <= dc <= 100.0)
+    print("Started in1")
     pwm_2.start(DC_MOTOR_OFF)
+    print("Started in2")
 
     time.sleep(5)
 
