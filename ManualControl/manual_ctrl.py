6370a6fb58deb69d97d11efccae1072b4bf18424
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-15 21:38:05 +0100

    xbox 360 controller python

--- ManualControl/manual_ctrl.py
+++ ManualControl/manual_ctrl.py
@@ -1,21 +1,39 @@
-import curses
+import signal
+from xbox360controller import Xbox360Controller
 
-def main(stdscr):
-    # Clear screen
-    stdscr.clear()
-    # Turn off echoing of keys, and enter cbreak mode,
-    # where no buffering is performed on keyboard input
-    curses.cbreak()
-    stdscr.keypad(True)
 
-    while True:
-        # Get the user's input
-        c = stdscr.getch()
-        stdscr.addch(c)
-        stdscr.refresh()
-        # Exit on 'q' key
-        if c == ord('q'):
-            break
+def on_button_pressed(button):
+    print(button)
+    print('Button {0} was pressed'.format(button.name))
 
-# Run the application
-curses.wrapper(main)
+
+def on_button_released(button):
+    print(button)
+    print('Button {0} was released'.format(button.name))
+
+
+def on_axis_moved(axis):
+    print(axis)
+    if "Raw" in str(axis):
+        print(f"Axis {axis.name} moved to {axis.value}")
+    else:
+        print(f"Axis {axis.name} moved to {axis.x} {axis.y}")    
+
+try:
+    with Xbox360Controller(0, axis_threshold=0.2) as controller:
+        controller.info()
+          
+        # Button A events
+        controller.button_a.when_pressed = on_button_pressed
+        controller.button_a.when_released = on_button_released
+                
+        # Left and right axis move event
+        controller.axis_l.when_moved = on_axis_moved
+        controller.axis_r.when_moved = on_axis_moved
+
+        controller.trigger_l.when_moved = on_axis_moved
+        controller.trigger_r.when_moved = on_axis_moved
+        
+        signal.pause()
+except KeyboardInterrupt:
+    pass
