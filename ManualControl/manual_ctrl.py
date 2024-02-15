a0a85ebb50d2bdc6c5ae7436db27ecf18d2ec9ae
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-15 21:48:33 +0100

    put print in while loop

--- ManualControl/manual_ctrl.py
+++ ManualControl/manual_ctrl.py
@@ -13,15 +13,16 @@ try:
     with Xbox360Controller(0, axis_threshold=0.2) as controller:
         controller.info()
 
-        print(controller.axis_l._value_x)
-        print(controller.axis_l._value_y)
+        while True:
+            print(controller.axis_l.x)
+            print(controller.axis_l.y)
 
-        #ser = serial.Serial('/dev/cu.usbserial-0001')  # open serial port
-        #ser.baudrate = 115200
-        #command = b"M50"        # Command to send
-        #ser.write(command)      # write to ESP32
+            #ser = serial.Serial('/dev/cu.usbserial-0001')  # open serial port
+            #ser.baudrate = 115200
+            #command = b"M50"        # Command to send
+            #ser.write(command)      # write to ESP32
 
-        #ser.close()             # close por
+            #ser.close()             # close por
 
         # Left and right axis move event
         # controller.axis_l.when_moved = on_axis_moved
