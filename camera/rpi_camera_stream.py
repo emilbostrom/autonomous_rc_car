36b360af080055c69b7a62c3916fede697565e16
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-20 21:57:44 +0100

    fixed missing camera code

--- camera/rpi_camera_stream.py
+++ camera/rpi_camera_stream.py
@@ -1,15 +1,20 @@
 #111!/usr/bin/env python
 # -*- coding: utf-8 -*
+
 #sudo apt-get install python3-flask
 #pip3 install opencv-python
+
 from flask import Flask, render_template, Response
 import cv2
+
 app = Flask(__name__)
 #app.config["CACHE_TYPE"] = "null"
+
 @app.route('/')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
+
 def gen():
     """Video streaming generator function."""
     vs = cv2.VideoCapture(0)
@@ -22,3 +27,11 @@ def gen():
         
     vs.release()
     cv2.destroyAllWindows() 
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
