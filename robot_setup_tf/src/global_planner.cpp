ee6a926e50e38f82ba75fb87b592b351b72576aa
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-29 20:40:46 +0100

    copied code from forum on how to handle PoseStamped

--- robot_setup_tf/src/global_planner.cpp
+++ robot_setup_tf/src/global_planner.cpp
@@ -2,7 +2,22 @@
 #include <geometry_msgs/PoseStamped.h>
 
 void positionSubscriber(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-    ROS_INFO("I heard: [%s]", msg->pose.c_str());
+    ROS_INFO_STREAM("Received pose: " << msg);
+    x_current = msg->pose.position.x;
+    y_current = msg->pose.position.y;
+    z_current = msg->pose.position.z;
+    x_quat = msg->pose.orientation.x;
+    y_quat = msg->pose.orientation.y;
+    z_quat = msg->pose.orientation.z;
+    w_quat = msg->pose.orientation.w;
+
+    ROS_INFO_STREAM(x_current);
+    ROS_INFO_STREAM(y_current);
+    ROS_INFO_STREAM(z_current);
+    ROS_INFO_STREAM(x_quat);
+    ROS_INFO_STREAM(y_quat);
+    ROS_INFO_STREAM(z_quat);
+    ROS_INFO_STREAM(w_quat);
 }
 
 int main(int argc, char** argv) {
