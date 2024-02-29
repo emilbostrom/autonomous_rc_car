e73569efc28f895886211549fa23727db69ed666
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-29 20:39:07 +0100

    syntax error fix for global planner

--- robot_setup_tf/src/global_planner.cpp
+++ robot_setup_tf/src/global_planner.cpp
@@ -2,13 +2,13 @@
 #include <geometry_msgs/PoseStamped.h>
 
 void positionSubscriber(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-    ROS_INFO("I heard: [%s]", msg->data.c_str());
+    ROS_INFO("I heard: [%s]", msg->pose.c_str());
 }
 
 int main(int argc, char** argv) {
 
     ros::init(argc,argv, "listener");
-    ros::Nodehandle n;
+    ros::NodeHandle n;
 
     ros::Subscriber sub = n.subscribe("chatter",1000,positionSubscriber);
 
