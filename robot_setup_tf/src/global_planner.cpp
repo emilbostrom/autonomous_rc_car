bb4bce77321551ca157eab9a0bbf5bb18de61b05
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-02-29 21:56:05 +0100

    added publisher of goal_pos

--- robot_setup_tf/src/global_planner.cpp
+++ robot_setup_tf/src/global_planner.cpp
@@ -13,6 +13,14 @@ double y_quat = 0;
 double z_quat = 0;
 double w_quat = 0;
 
+double x_goal = 5;
+double y_goal = 5;
+double z_goal = 0;
+double x_quat_goal = 0.707;
+double y_quat_goal = 0
+double z_quat_goal = 0;
+double w_quat_goal = 0.707;
+
 void positionSubscriber(const geometry_msgs::PoseStamped::ConstPtr& msg) {
     ROS_INFO_STREAM("Received pose: " << msg);
     x_current = msg->pose.position.x;
@@ -34,12 +42,31 @@ void positionSubscriber(const geometry_msgs::PoseStamped::ConstPtr& msg) {
 
 int main(int argc, char** argv) {
 
-    ros::init(argc,argv, "listener");
+    ros::init(argc,argv, "global_planner");
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("slam_out_pose",1000,positionSubscriber);
+    ros::Rate r(10); // 10 hz
+    while (should_continue)
+    {
+        ros::Subscriber sub = n.subscribe("slam_out_pose",1000,positionSubscriber);
+        
+        ros::Publisher pub = n.advertise<geometry_msgs::PoseStamped("goal_position",10);
+        
+        geometry_msgs::PoseStamped goal_pos_msg;
+        goal_pos_msg.pose.position.x = x_goal;
+        goal_pos_msg.pose.position.y = y_goal;
+        goal_pos_msg.pose.position.z = z_goal;
+        goal_pos_msg.pose.orientation.x = x_quat_goal;
+        goal_pos_msg.pose.orientation.y = y_quat_goal;
+        goal_pos_msg.pose.orientation.z = z_quat_goal;
+        goal_pos_msg.pose.orientation.w = w_quat_goal;
 
-    ros::spin();
+        pub.publish(goal_pos_msg);
+
+        ros::spinOnce();
+        r.sleep();
+    }
+
     
     return 0;
 
