64ce2b3b2c95ba14817e24c9c87236841f7c7d3e
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-03-03 16:53:44 +0100

    test

--- robot_setup_tf/src/global_planner.cpp
+++ robot_setup_tf/src/global_planner.cpp
@@ -133,7 +133,7 @@ class GlobalPlanner{
                 if (mapData[i] != -1) {
                     ROS_INFO_STREAM("index: "<< i);
                     ROS_INFO_STREAM("value: "<< mapData[i]);
-                    ROS_INFO_STREAM("x: " << mapData[i])
+                    ROS_INFO_STREAM("y: " << mapData[i] % 2048 - 1024*0.05);
                 }
             }
 
