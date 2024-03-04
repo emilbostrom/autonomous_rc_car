bf6fc944e07620983c8be97f2104f76fffea038e
emilbostrom <81952201+emilbostrom@users.noreply.github.com>
2024-03-04 21:06:16 +0100

    syntax fix

--- robot_setup_tf/src/global_planner.cpp
+++ robot_setup_tf/src/global_planner.cpp
@@ -306,8 +306,8 @@ int main(int argc, char** argv) {
     boost::shared_ptr<nav_msgs::OccupancyGrid const> mapDataMsg;
     mapDataMsg = ros::topic::waitForMessage<nav_msgs::OccupancyGrid>("map",ros::Duration(2.0));
 
-    boost::shared_ptr<geometry_msgs::PoseStamped const> goalPosMsg;
-    goalPosMsg = ros::topic::waitForMessage<geometry_msgs::PoseStamped>("move_base_simple/goal",ros::Duration(30.0));
+    boost::shared_ptr<geometry_msgs::Pose const> goalPosMsg;
+    goalPosMsg = ros::topic::waitForMessage<geometry_msgs::Pose>("move_base_simple/goal",ros::Duration(30.0));
 
     GlobalPlanner planner(iniPosMsg,mapDataMsg,goalPosMsg);
 
